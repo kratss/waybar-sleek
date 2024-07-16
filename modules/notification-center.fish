6be64b83cdf08ec3951edf57e825abe39a6a3d62
krats <krats@noreply.codeberg.org>
2024-07-16 22:43:39 +0200

    Unused modules deleted

--- modules/notification-center.fish
+++
@@ -1,6 +0,0 @@
-#!/bin/env fish
-set notifs $(makoctl history)
-set notifs_jq $(echo $notifs | jq '.data.[].[] | [.summary.data, .body.data]')
-set notifs_parsed $(echo $notifs_jq | jq -s 'map(., "\n") | .[]')
-echo "{"text": "$notifs_parsed"}"
-
